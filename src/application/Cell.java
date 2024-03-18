693742c6715f13ae07f47c100904fabaf9032bc8
Justin Foll <jfoll@iu.edu>
2024-03-18 12:44:49 -0400

    corrected Cell

--- src/application/Cell.java
+++ src/application/Cell.java
@@ -1,16 +1,35 @@
 package application;
 
 public class Cell {
-
-	private enum State {
-		HIDDEN,
-		REVEALED
-	}
 	
-	State _state;
+	boolean _isHidden;
+	
+	boolean _isMine;
 	
 	public void Cell() {
-		_state = State.HIDDEN;
+		_isHidden = true;
+		_isMine = false;
+	}
+	
+	private void changeHiddenStatus() {
+		if(_isHidden) {
+			_isHidden = false;
+			return;
+		}
+		_isHidden = true;
+	}
+	
+	public boolean isHidden() {
+		return _isHidden;
+	}
+	
+	public void setMine() {
+		_isMine = true;
+	}
+	
+	public boolean isMine() {
+		return _isMine;
 	}
 	
 }
+
