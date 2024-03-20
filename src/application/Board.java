abe8202aa280e1e0424623a31dff24adb5c9e5ba
zjdugan <zjdugan@iulb-if0006-03.ads.iu.edu>
2024-03-20 12:26:40 -0400

    Updated Board, tracking numHidden

--- src/application/Board.java
+++ src/application/Board.java
@@ -1,28 +1,36 @@
 package application;
 
 public class Board {
-	Cell[][] _board = new Cell[10][10];
+	static Cell[][] _board = new Cell[10][10];
 	
 	int mines = 10;
 	
 	public void buildBoard() {
 		for (int row = 0; row < 10; row++) {
 			for (int col = 0; col < 10; col++) {
-				_board[row][col] = new Cell();
+				_board[row][col] = new Cell(row, col);
 			}
 		}
 	}
 	
-	public boolean checkBoard() {
+	public void checkBoard() {
+		int numHidden = 0;
 		for (int row = 0; row < 10; row++) {
 			for (int col = 0; col < 10; col++) {
 				if (_board[row][col].isHidden()) {
-					return false;
+					numHidden += 1;
+					if (numHidden > mines) return;
 				}
 			}
-		}
-		return true;
+		} 
+		if (numHidden == mines) endGame();
 	}
+	
+	private void revealCell(int x, int y) {
+		
+	}
+	
+	
 	
 }
 
